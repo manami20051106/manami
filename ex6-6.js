aebf1eabe7ed56fd263520de448a93d1cb66f645
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-10-29 09:29:51 +0900

    Update ex6-6.js

--- ex6-6.js
+++ ex6-6.js
@@ -2,11 +2,11 @@ var who66 ='world';
 function sayhello66(){
 alert('Hello,'+who66+'!');
 }
-var who66 ='が来た';
+var who66 ='太郎';
 function taro66(){
-alert('太郎,'+who66+'!');
+alert('who66,'+が来た+'!');
 }
-var who66 ='が来た';
+var who66 ='花子';
 function hanako66(){
-alert('花子,'+who66+'!');
+alert('who66,'+が来た+'!');
 }
