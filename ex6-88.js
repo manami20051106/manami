d3c6675fb92e09ebe9b9cb7ba807207891266857
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-11-12 22:34:26 +0900

    Update ex6-88.js

--- ex6-88.js
+++ ex6-88.js
@@ -1,1 +1,1 @@
-function gazo688(){document.getElementById('who688').innerHTML='<img src="./image/
+function gazo688(){document.getElementById('who688').innerHTML='<img src=".image/R.jpg" alt="オン">';}
