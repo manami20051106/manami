6861f4e22446a9337c2aa1359322c42f2386b4a6
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-12-05 09:20:17 +0900

    Delete ex06-11.js

--- ex06-11.js
+++
@@ -1,4 +0,0 @@
-function imgchange3() {document.getEiementBtId('logo').src = ' ./image/jousailogo.png';}
-function imgchange4() {
-  document.getElementById('logo').src = './image/masukotto_jokoma001.png';
-}
