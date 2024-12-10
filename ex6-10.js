6e66c0730c6e9b0814c20307f4cf82f703a74ead
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-12-10 09:54:15 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,2 +1,2 @@
-function imgchange1(){document.getElementById('mark').src='.}
+function imgchange1(){document.getElementById('mark').src='.image/jousailogo.png';}
 function imgchange2(){document.getElementById('mark').src='.}
