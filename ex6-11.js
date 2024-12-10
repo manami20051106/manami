57d0a0adbece82917f1e41f17483209f7380b5ab
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-12-10 10:16:01 +0900

    Update ex6-11.js

--- ex6-11.js
+++ ex6-11.js
@@ -1,1 +1,7 @@
+function imgchange3() {
+    document.getElementById('logo').src = './image/jokoma.jpg';
+}
+function imgchange4() {
+    document.getElementById('logo').src = './image/josai.jpg';
+}
 
