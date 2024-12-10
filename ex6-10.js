3b114b7750ffa3d128f12476bc54c5fa1529fb1e
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-12-10 09:51:19 +0900

    Update ex6-10.js

--- ex6-10.js
+++ ex6-10.js
@@ -1,6 +1,2 @@
-function imgchange1() {
-  document.getElementById('logo').src = './image/masukotto_jokoma001.png';
-}
-function imgchange2() {
-document.getElementById('logo').src = './image/jousailogo.png';
-}
+function imgchange1(){document.getElementById('mark').src='.}
+function imgchange2(){document.getElementById('mark').src='.}
