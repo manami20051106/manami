1264253edb8f570a89a49915d0d527183fd0f1c1
manami20051106 <167833068+manami20051106@users.noreply.github.com>
2024-12-17 10:36:25 +0900

    Update ex7-11.js

--- ex7-11.js
+++ ex7-11.js
@@ -1,19 +1,4 @@
-function isLeapYear(year) {
-   if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
-       return true; // うるう年
-   } else {
-       return false; // 平年
-   }
-}
-// ユーザーから年を入力してもらう
-let year = prompt("西暦年を入力してください:");
-year = parseInt(year); // 数字に変換
-if (isNaN(year)) {
-   alert("正しい年を入力してください。");
-} else {
-   if (isLeapYear(year)) {
-       alert(year + "年はうるう年です。");
-   } else {
-       alert(year + "年は平年です。");
-   }
-}
+function year(){var e=document.getElementById('year1').value;
+                if(Number(e)%100==0){alert('平年');}
+              　else if(Number(e)%4==0){alert('閏年');}
+              　else {alert('平年');}}
